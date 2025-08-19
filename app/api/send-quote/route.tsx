1f81c3d77837cb741e8c753613ccec4b81697975
Riccardo Fusco <90351358+rfusco22@users.noreply.github.com>
2025-08-19 02:41:29 -0400

    Update route.tsx

--- app/api/send-quote/route.tsx
+++ app/api/send-quote/route.tsx
@@ -1,5 +1,6 @@
 import { Resend } from "resend"
 import { type NextRequest, NextResponse } from "next/server"
+import { formatInTimeZone } from 'date-fns-tz'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
